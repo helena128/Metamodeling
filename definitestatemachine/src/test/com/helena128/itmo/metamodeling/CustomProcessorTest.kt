86aaedc679fded18dc2b7b364d47dbf8bd1e03a1
Elena Tcheprasova <hellogbel@gmail.com>
2020-09-29 20:59:51 +0300

    Added ignored files

---
+++ definitestatemachine/src/test/com/helena128/itmo/metamodeling/CustomProcessorTest.kt
@@ -0,0 +1,27 @@
+package com.helena128.itmo.metamodeling
+
+import org.junit.Test
+import kotlin.test.assertTrue
+
+class CustomProcessorTest {
+
+    @Test
+    fun testAPath() {
+        assertTrue { isInputAcceptedByPreDefinedDSM("aaabc") }
+        assertTrue { isInputAcceptedByPreDefinedDSM("abc") }
+    }
+
+    @Test
+    fun testBPath() {
+        assertTrue { isInputAcceptedByPreDefinedDSM("bbc") }
+        assertTrue { isInputAcceptedByPreDefinedDSM("bbbbbc") }
+        assertTrue { isInputAcceptedByPreDefinedDSM("bc") }
+    }
+
+    @Test
+    fun testCPath() {
+        assertTrue { isInputAcceptedByPreDefinedDSM("ccbc") }
+        assertTrue { isInputAcceptedByPreDefinedDSM("cbc") }
+        assertTrue { isInputAcceptedByPreDefinedDSM("ccbc") }
+    }
+}
